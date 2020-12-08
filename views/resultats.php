c7afa51fd1be7ea097b5f4603eeaea46827a7006
Matthieu BOUCHET <matthieu.bouchet@outlook.com>
2020-12-08 22:48:29 +0100

    Mise à jour des champs
    Correction de l'affichage des résultats et de la table légumes
    
    Co-Authored-By: lunopa <63592758+lunopa@users.noreply.github.com>

--- views/resultats.php
+++ views/resultats.php
@@ -23,7 +23,31 @@ $t_1 = "<table class='mt-5 mw-100 table w-100'>
             </thead>
             <tbody>";
 
-$t_other = "<table class='mt-5 mw-100 table w-100'>
+$t_legumes = "<table class='mt-5 mw-100 table w-100'>
+<thead>
+<tr>
+<th>Espèce</th>
+<th>Variété</th>
+<th>Nom latin</th>
+<th>Stock (unités)</th>
+<th>Famille</th>
+<th>Cycle</th>
+<th>Couleur</th>
+<th>Récolte</th>
+<th>Hauteur (cm)</th>
+<th>Semis</th>
+<th>Type semis</th>
+<th>Ecartement entre lignes (cm)</th>
+<th>Ecartement sur lignes (cm)</th>
+<th>Technique</th>
+<th>Exposition</th>
+<th>Pollinisateur</th>
+<th>Infos</th>
+</tr>
+</thead>
+<tbody>";
+
+$t_other = "<table class='mt-5 mw-100 table w-100'> <!--Changements sur récolte et ajout des champs entre lignes-->
 <thead>
 <tr>
 <th>Espèce</th>
@@ -82,6 +106,35 @@ $t_other = "<table class='mt-5 mw-100 table w-100'>
                             </tr>";
 
                             echo($t_1_val);
+                        }
+                    }
+                    else if($key==3){ //Légumes
+                        echo($t_legumes);
+
+                        foreach($resultats[$key] as $ligne)
+                        {
+                            $ligne=decode_donnees($ligne);
+                            $t_val_legumes = "<tr>
+                                <th>".$ligne['espece']."</th>
+                                <th>".$ligne['variete']."</th>
+                                <td><i>".$ligne['latin']."</i></td>
+                                <td>".$ligne['stock']."</td>
+                                <td>".$ligne['famille']."</td>      
+                                <td>".$ligne['cycle']."</td>
+                                <td>".$ligne['couleur']."</td>
+                                <td>".$ligne['debut_floraison']." - ".$ligne['fin_floraison']."</td>
+                                <td>".$ligne['hauteur']."</td>
+                                <td>".$ligne['debut_semis']." - ".$ligne['fin_semis']."</td>
+                                <td>".$ligne['type_semis']."</td>
+                                <td>".$ligne['ecartement_entre_lignes']."</td>
+                                <td>".$ligne['ecartement_sur_lignes']."</td>
+                                <td>".$ligne['technique']."</td>
+                                <td>".$ligne['exposition']."</td>
+                                <td>".$ligne['pollinisateur']."</td>
+                                <th>".$ligne['infos']."</th>
+                                </tr>";
+
+                            echo($t_val_legumes);
                         }
                     }
                     else{
