d9f0b21a822b193ef134478d1d47a5eb3f4c87f9
Matthieu BOUCHET <matthieu.bouchet@outlook.com>
2020-12-12 23:16:31 +0100

    Update grainotheque.sql

--- grainotheque.sql
+++ grainotheque.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 11 déc. 2020 à 17:40
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Généré le :  sam. 12 déc. 2020 à 22:15
+-- Version du serveur :  8.0.18
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `grainotheque`
+-- Base de données :  `grainotheque`
 --
 
 -- --------------------------------------------------------
@@ -30,10 +31,10 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `aromatiques`;
 CREATE TABLE IF NOT EXISTS `aromatiques` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `espece` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `espece` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `stock` smallint(5) DEFAULT NULL,
-  `latin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `famille` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latin` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `famille` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `cycle` tinyint(1) DEFAULT NULL,
   `couleur` tinyint(4) DEFAULT NULL,
   `debut_floraison` tinyint(1) DEFAULT NULL,
@@ -41,11 +42,11 @@ CREATE TABLE IF NOT EXISTS `aromatiques` (
   `hauteur` tinyint(4) DEFAULT NULL,
   `debut_semis` tinyint(1) DEFAULT NULL,
   `fin_semis` tinyint(1) DEFAULT NULL,
-  `type_semis` text COLLATE utf8_unicode_ci,
-  `technique` text COLLATE utf8_unicode_ci,
+  `type_semis` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `technique` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `exposition` tinyint(1) DEFAULT NULL,
   `pollinisateur` tinyint(1) DEFAULT NULL,
-  `infos` text COLLATE utf8_unicode_ci,
+  `infos` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -54,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `aromatiques` (
 --
 
 INSERT INTO `aromatiques` (`id`, `espece`, `stock`, `latin`, `famille`, `cycle`, `couleur`, `debut_floraison`, `fin_floraison`, `hauteur`, `debut_semis`, `fin_semis`, `type_semis`, `technique`, `exposition`, `pollinisateur`, `infos`) VALUES
-(1, 'Aneth odorant', 3, 'Anethum graveolens', 'Apiacées', 1, 1, 6, 10, 100, 4, 5, 'En place (ne supporte pas le repiquage)', '', 1, 1, '');
+(1, 'Aneth odorant', 6, 'Anethum graveolens', 'Apiacées', 1, 1, 6, 10, 100, 4, 5, NULL, NULL, 1, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -65,10 +66,10 @@ INSERT INTO `aromatiques` (`id`, `espece`, `stock`, `latin`, `famille`, `cycle`,
 DROP TABLE IF EXISTS `fleurs_horticoles`;
 CREATE TABLE IF NOT EXISTS `fleurs_horticoles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `espece` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `espece` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `stock` smallint(5) DEFAULT NULL,
-  `latin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `famille` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latin` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `famille` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `cycle` tinyint(1) DEFAULT NULL,
   `couleur` tinyint(4) DEFAULT NULL,
   `debut_floraison` tinyint(1) DEFAULT NULL,
@@ -76,11 +77,11 @@ CREATE TABLE IF NOT EXISTS `fleurs_horticoles` (
   `hauteur` tinyint(4) DEFAULT NULL,
   `debut_semis` tinyint(1) DEFAULT NULL,
   `fin_semis` tinyint(1) DEFAULT NULL,
-  `type_semis` text COLLATE utf8_unicode_ci,
-  `technique` text COLLATE utf8_unicode_ci,
+  `type_semis` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `technique` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `exposition` tinyint(1) DEFAULT NULL,
   `pollinisateur` tinyint(1) DEFAULT NULL,
-  `infos` text COLLATE utf8_unicode_ci,
+  `infos` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -93,10 +94,10 @@ CREATE TABLE IF NOT EXISTS `fleurs_horticoles` (
 DROP TABLE IF EXISTS `fleurs_sauvages_locales`;
 CREATE TABLE IF NOT EXISTS `fleurs_sauvages_locales` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `espece` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `espece` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `stock` smallint(5) DEFAULT NULL,
-  `latin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `famille` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latin` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `famille` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `cycle` tinyint(1) DEFAULT NULL,
   `couleur` tinyint(4) DEFAULT NULL,
   `debut_floraison` tinyint(1) DEFAULT NULL,
@@ -104,23 +105,25 @@ CREATE TABLE IF NOT EXISTS `fleurs_sauvages_locales` (
   `hauteur` tinyint(4) DEFAULT NULL,
   `debut_semis` tinyint(1) DEFAULT NULL,
   `fin_semis` tinyint(1) DEFAULT NULL,
+  `type_semis` text COLLATE utf8_unicode_ci NOT NULL,
   `culture` tinyint(1) DEFAULT NULL,
-  `technique` text COLLATE utf8_unicode_ci,
+  `technique` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `exposition` tinyint(1) DEFAULT NULL,
   `pollinisateur` tinyint(1) DEFAULT NULL,
-  `infos` text COLLATE utf8_unicode_ci,
+  `infos` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `fleurs_sauvages_locales`
 --
 
-INSERT INTO `fleurs_sauvages_locales` (`id`, `espece`, `stock`, `latin`, `famille`, `cycle`, `couleur`, `debut_floraison`, `fin_floraison`, `hauteur`, `debut_semis`, `fin_semis`, `culture`, `technique`, `exposition`, `pollinisateur`, `infos`) VALUES
-(1, 'Achillée Millefeuille', 7, 'Achillea millefolium', 'Astéracées', 3, 1, 6, 9, 50, 5, 7, 1, 'Privilégier repiquages repousses', 3, 1, ''),
-(2, 'Aigremoine eupatoire', 7, 'Agremonia eupatoria', 'Rosacées', 3, 2, 6, 9, 50, 8, 9, 0, 'Zoochorie', 1, 1, ''),
-(3, 'Agripaume cardiaque', NULL, 'Leonurus cardiaca', 'Lamiacées', 3, 7, 6, 8, NULL, NULL, NULL, NULL, NULL, 2, 1, NULL),
-(4, 'Alliaire officinale', NULL, 'Allira officinalis', 'Brassicacées', 2, NULL, 4, 6, NULL, 6, 9, NULL, NULL, 2, 1, NULL);
+INSERT INTO `fleurs_sauvages_locales` (`id`, `espece`, `stock`, `latin`, `famille`, `cycle`, `couleur`, `debut_floraison`, `fin_floraison`, `hauteur`, `debut_semis`, `fin_semis`, `type_semis`, `culture`, `technique`, `exposition`, `pollinisateur`, `infos`) VALUES
+(1, 'Achillée Millefeuille', 10, 'Achillea millefolium', 'Astéracées', 3, 1, 6, 9, 50, 5, 7, '', 1, 'Privilégier repiquages repousses', 3, 1, 'Modification'),
+(2, 'Aigremoine eupatoire', 7, 'Agremonia eupatoria', 'Rosacées', 3, 2, 6, 9, 50, 8, 9, '', 0, 'Zoochorie', 1, 1, ''),
+(3, 'Agripaume cardiaque', 3, 'Leonurus cardiaca', 'Lamiacées', 3, 7, 6, 8, 10, 1, 3, '', NULL, NULL, 2, 1, NULL),
+(4, 'Alliaire officinale', 10, 'Allira officinalis', 'Brassicacées', 2, NULL, 4, 6, NULL, 6, 9, '', NULL, NULL, 2, 1, NULL),
+(5, 'Anthyllide vulnéraire', 0, 'Anthyllis vulneraria', 'Fabacées', 3, 2, 6, 9, NULL, 3, 4, '', NULL, NULL, 1, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -131,11 +134,11 @@ INSERT INTO `fleurs_sauvages_locales` (`id`, `espece`, `stock`, `latin`, `famill
 DROP TABLE IF EXISTS `legumes`;
 CREATE TABLE IF NOT EXISTS `legumes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `espece` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `espece` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `stock` smallint(5) DEFAULT NULL,
-  `variete` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `latin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `famille` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `variete` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `latin` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `famille` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `cycle` tinyint(1) DEFAULT NULL,
   `couleur` tinyint(4) DEFAULT NULL,
   `debut_floraison` tinyint(1) DEFAULT NULL,
@@ -143,13 +146,13 @@ CREATE TABLE IF NOT EXISTS `legumes` (
   `hauteur` tinyint(4) DEFAULT NULL,
   `debut_semis` tinyint(1) DEFAULT NULL,
   `fin_semis` tinyint(1) DEFAULT NULL,
-  `type_semis` text COLLATE utf8_unicode_ci,
+  `type_semis` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `ecartement_entre_lignes` float DEFAULT NULL,
   `ecartement_sur_lignes` float DEFAULT NULL,
-  `technique` text COLLATE utf8_unicode_ci,
+  `technique` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `exposition` tinyint(1) DEFAULT NULL,
   `pollinisateur` tinyint(1) DEFAULT NULL,
-  `infos` text COLLATE utf8_unicode_ci,
+  `infos` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -158,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `legumes` (
 --
 
 INSERT INTO `legumes` (`id`, `espece`, `stock`, `variete`, `latin`, `famille`, `cycle`, `couleur`, `debut_floraison`, `fin_floraison`, `hauteur`, `debut_semis`, `fin_semis`, `type_semis`, `ecartement_entre_lignes`, `ecartement_sur_lignes`, `technique`, `exposition`, `pollinisateur`, `infos`) VALUES
-(1, 'Aubergine', 5, '', 'Solanum melongena', 'Astéridées', 1, 7, 7, 10, 80, 2, 5, '2/3 graines par pot', 50, 0, 'Recouvrir d\'une terre légère. Arroser régulièrement', 4, 0, '');
+(1, 'Aubergine', 5, '', 'Solanum melongena', 'Astéridées', 1, 7, 7, 10, 80, 2, 5, NULL, 50, 2, 'Recouvrir d\'une terre légère. Arroser régulièrement', 4, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
