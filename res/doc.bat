a25bdb113024017c27409904db0610838c5b6624
Matthieu BOUCHET <matthieu.bouchet@outlook.com>
2020-12-16 18:43:48 +0100

    Ajout de la Doc

--- res/doc.bat
+++ res/doc.bat
@@ -1,2 +1,2 @@
-php ./modules_ext/phpDocumentor.phar -d ../src/ -t ./docs
+php ./modules_ext/phpDocumentor.phar run --ignore D:\Programmation\Dev_Web\vhosts\grainotheque\src\include\* -d D:\Programmation\Dev_Web\vhosts\grainotheque\src -t ./docs
 timeout 1000
